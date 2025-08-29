2b515c9a6a0e3cfe52b1657c2546385e5521dfa3
Kenneth Tay <kenneth21tay@gmail.com>
2025-08-29 23:07:31 +0800

    First commit

---
+++ src/main/java/com/chitchat/chit_chat/ChitChatApplication.java
@@ -0,0 +1,13 @@
+package com.chitchat.chit_chat;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChitChatApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ChitChatApplication.class, args);
+	}
+
+}
