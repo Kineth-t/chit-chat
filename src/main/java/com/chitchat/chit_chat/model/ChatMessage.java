3f858796987257073d2c9ddb4ee0b6768f1fabf9
Kenneth Tay <kenneth21tay@gmail.com>
2025-08-29 23:57:54 +0800

    Add ChatMessage model class

---
+++ src/main/java/com/chitchat/chit_chat/model/ChatMessage.java
@@ -0,0 +1,12 @@
+package com.chitchat.chit_chat.model;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Table;
+import lombok.Data;
+
+@Data
+@Entity
+@Table(name = "chat_messages")
+public class ChatMessage {
+    
+}
