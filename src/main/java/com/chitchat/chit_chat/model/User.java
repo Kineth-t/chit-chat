0d15e136b5fdeacdd4f54718a7297f074f2d2b79
Kenneth Tay <kenneth21tay@gmail.com>
2025-08-29 23:55:30 +0800

    New model added(user)

---
+++ src/main/java/com/chitchat/chit_chat/model/User.java
@@ -0,0 +1,29 @@
+package com.chitchat.chit_chat.model;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.Data;
+
+@Entity
+@Data
+@Table(name = "users")
+public class User {
+    @Id
+    @GeneratedValue
+    private Long id;
+
+    @Column(unique = true, nullable = false)
+    private String username;
+
+    @Column(nullable = false)
+    private String password;
+
+    @Column(unique = true, nullable = false)
+    private String email;
+
+    @Column(nullable = false, name = "is_online")
+    private boolean isOnline;
+}
