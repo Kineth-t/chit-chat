f9fbd46b53f9d5ef1c2837f9cdf5291a6fc368b8
Kenneth Tay <kenneth21tay@gmail.com>
2025-09-02 15:56:56 +0800

    Rename class name with something more accurate and change error thrown to be a more specific error.

--- src/main/java/com/chitchat/chit_chat/service/CustomUserDetailsService.java
+++ src/main/java/com/chitchat/chit_chat/service/CustomUserDetailsService.java
@@ -10,15 +10,19 @@ import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import com.chitchat.chit_chat.model.User;
 import com.chitchat.chit_chat.repository.UserRepository;
 
-public class CustomUserDetails implements UserDetailsService {
+// Custom implementation of the Spring Security UserDetailsService interface
+// It's part of Spring Boot authentication setup, where user information is loaded from a database for login and security purposes.
+
+public class CustomUserDetailsService implements UserDetailsService {
     @Autowired
     private UserRepository userRepository;
 
     @Override
     public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
         User user = userRepository.findByUsername(username)
-                        .orElseThrow(() -> new RuntimeException("User not found"));
+                        .orElseThrow(() -> new UsernameNotFoundException("User not found with username: " + username));
 
+        // Converts own User object to a Spring Security User object.
         return org.springframework.security.core.userdetails.User.builder()
                     .username(user.getUsername())
                     .password(user.getPassword())
